d50745b805e7b1495d5763a46c6ff6bddd2cfa8b
Auss Abbood <aussabbood@Ausss-MacBook-Pro.local>
2018-11-22 11:01:50 +0100

    Restructures folder. Notebooks go into /archive, pickles into /pickles and unnecessary files were deleted

--- datenbank_clean.py
+++ datenbank_clean.py
@@ -1,7 +1,7 @@
 import re
 import pandas as pd
 from datetime import datetime
-
+from wiki_country_parser import get_wiki_countries_df
 
 def edb_to_timestamp(pd_data):
     """Transforms an unconverted string in pandas DataFrame or Series of Ereignisdatenbank (edb) to timestamp"""
@@ -47,3 +47,26 @@ def clean_country_names(countries):
     # "Recursively" clean lists
     countries_unique = list(map(lambda x: clean_country_names(x) if type(x) == list else x, countries_unique))
     return countries_unique
+
+
+def translate_abbreviation(to_translate):
+    """Takes a list of countries and/or abbreviations and translates the abbreviations to the full state name"""
+    wikipedia_country_list = get_wiki_countries_df()
+    to_return = []
+    if type(to_translate) == str:
+        to_translate = [to_translate]
+    for potential_abbreviation in to_translate:
+        if type(potential_abbreviation) == str and not re.findall(r"([^A-Z]+)", potential_abbreviation):
+
+            # First check the official abrev. than the self created ones e.g. VAE for the Emirates
+            for column in ["wiki_abbreviations", "inoff_abbreviations"]:
+                for i, abbreviation in enumerate(wikipedia_country_list[column]):
+                    if potential_abbreviation in abbreviation:
+                        to_return.append(wikipedia_country_list["state_name_de"].tolist()[i])
+        elif type(potential_abbreviation) == list:
+            list_entry = [translate_abbreviation(nested_entry) for nested_entry in potential_abbreviation]
+            flattened = [entry for sublist in list_entry for entry in sublist]
+            to_return.append(flattened)
+        else:
+            to_return.append(potential_abbreviation)
+    return to_return
