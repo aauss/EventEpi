989cf442863fb23d8c744f4af51269f71195b65f
Auss Abbood <aussabbood@Ausss-MacBook-Pro.local>
2018-11-22 11:51:49 +0100

    Takes into action what I unfortunatly did manually and without git rm, git mv

--- wiki_country_parser_test.py
+++
@@ -1,6 +0,0 @@
-from geoname_translator import *
-print("Start testing parsed Wikipedia country list data frame...")
-assert get_wiki_countries_df().iloc[11].tolist() == ['Andorra', 'Fürstentum Andorra', 'Andorra la Vella',
-                                                     'Andorra', ['AND', 'AD'], ['FA']], "Unexpected output of wiki data"
-print("...testing parsed Wikipedia country list completed.")
-print("Testing Wikipedia scraper is completed.")
