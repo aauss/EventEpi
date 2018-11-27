1b22e8d2d1f44b6c3602eb3fd1fe2e498b91e4b1
Auss Abbood <aussabbood@Ausss-MacBook-Pro.local>
2018-11-27 12:05:01 +0100

    Adds function to load edb and use boilerpipe to extract links

--- my_utils.py
+++ my_utils.py
@@ -1,3 +1,9 @@
+import pandas as pd
+from tqdm import tqdm
+from boilerpipe.extract import Extractor
+from edb_clean import *
+
+
 def flatten_list(list_2d):
     # TODO: See whether this also could handle deeper nesting e.g. ["USA,["BLA",["a","b"]],"U]
     """Takes a nested list and returns a flattened list."""
@@ -9,3 +15,26 @@ def flatten_list(list_2d):
         else:
             flattened.extend(flatten_list(entry))
     return flattened
+
+
+def extract_from_url(list_of_links):
+    """Extracts the main content from a list of links and returns a list of texts (str)
+
+    list_of_links -- a list containing URLs of webpages to get the main content from
+    """
+    if type(list_of_links) == str:
+        list_of_links = [list_of_links]
+    return[Extractor(extractor='ArticleExtractor', url=url).getText().replace('\n', '') for url in tqdm(list_of_links)]
+
+
+def get_edb():
+    """
+
+    Returns:
+        pd.DataFrame: formatted edb
+    """
+    edb = pd.read_csv("edb.csv", sep=";")
+    edb = edb.dropna(how="all").reset_index(drop=True)
+    edb.columns = list(map(lambda x: x.strip(" "), edb.columns))
+    edb
+    return edb
