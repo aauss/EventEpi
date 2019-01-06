3f804e6fa20fa194cccca048304f60f4a8b6a8e8
Auss <a.abbood@live.de>
2019-01-06 17:13:19 +0100

    Simplify logic of reduced and unprocessed in get_cleaned_edb and remove unsused parameter. Move translate_disease_name to other file, replace doctstrings with single line comments, and chang order of functions

--- nlp_surveillance/edb_clean.py
+++ nlp_surveillance/edb_clean.py
@@ -8,14 +8,40 @@ import pandas as pd
 from didyoumean import didyoumean
 from datetime import datetime
 from .wiki_country_parser import get_wiki_countries_df
-from .wiki_disease_parser import get_wiki_disease_df
+from .wiki_disease_parser import translate_disease_name
 
 
-def _add_zero(x):
-    # Adds a zero if day or month only have a single value
-    if len(x) == 1:
-        x = "0" + x
-    return x
+def get_cleaned_edb(clean=None, reduced=True, unprocessed=False, path=None):
+    """
+
+    Returns:
+        pd.DataFrame: formatted edb
+        if reduce drop columns unnecessary for analysis
+    """
+    if clean is None:
+        clean = [(edb_to_timestamp, [7, 8, 10, 16, 19, 22, 25, 34]),
+                 (translate_geonames, [3, 4]),
+                 (translate_disease_name, [6])]
+    if not isinstance(clean, list):
+        clean = [clean]
+    edb = read_cleaned_edb(path=path)
+    if not unprocessed:
+        for funct, columns in clean:
+            edb.iloc[:, columns] = edb.iloc[:, columns].applymap(funct)
+    if reduced:
+        to_drop = edb.iloc[:, [0, 1, 2, 5, 7, 8, 27, 28, 29, 30, 31, 32, 33, 34, 35]].columns.tolist()
+        edb = edb.drop(to_drop, axis=1)
+    return edb
+
+
+def read_cleaned_edb(path=None):
+    if path is None:
+        dirname = os.path.dirname(__file__)
+        path = os.path.join(dirname, 'data', 'edb.csv')
+    edb = pd.read_csv(path, sep=";")
+    edb = edb.dropna(how="all").reset_index(drop=True)
+    edb.columns = list(map(lambda x: x.strip(), edb.columns))
+    return edb
 
 
 def edb_to_timestamp(date):
@@ -35,70 +61,15 @@ def edb_to_timestamp(date):
     return date
 
 
-def _complete_partial_words(to_complete, list_of_possible_corrections_de, list_of_possible_corrections_en):
-    # Takes a list of correctly written en/ger disease names and tries to complete them: Ebola -> Ebolavirus
-    match = [s for s in list_of_possible_corrections_de if s.lower().startswith(to_complete.lower())]
-    if not match:
-        match = [s for s in list_of_possible_corrections_en if s.lower().startswith(to_complete.lower())]
-    if match:
-        return match[0]
-    else:
-        return to_complete
-
-
-def translate_disease_name(disease):
-    # TODO: Write this into a wiki_disease_parser
-    # Initialization of databases
-    disease_translation_df, disease_code_df = get_wiki_disease_df()
-    disease_db_en = disease_translation_df['itemLabel_EN']
-    disease_db_de = [d for d in disease_translation_df['itemLabel_DE'] if isinstance(d, str)]
-
-    # Translate
-    disease = str(disease)
-    disease = disease.strip()
-    match_for_abbreviation = re.match(r'[A-Z]{2,}', disease)
-
-    if match_for_abbreviation:
-        index_for_matched_abbreviation = disease_code_df[disease_code_df['Code'] == match_for_abbreviation[0]]
-
-        if not index_for_matched_abbreviation.empty:
-            disease = index_for_matched_abbreviation['TypeName'].tolist()[0]
-
-    if disease in disease_db_de:
-        return disease_translation_df['itemLabel_EN'].loc[disease_translation_df['itemLabel_DE'] == disease].tolist()[0]
-    elif disease in disease_db_en:
-        return disease
-    elif ',' in disease:
-        # Did not translate because it is a concatenation of disease names
-        disease = disease.split(',')
-        disease = [entry.strip() for entry in disease]
-        return [translate_disease_name(entry) for entry in disease]
-
-    else:
-        # If typo occurred
-        did_u_mean = didyoumean.didYouMean(disease, disease_db_de)
-        if did_u_mean in disease_db_en:
-            return did_u_mean
-        elif did_u_mean in disease_db_de:
-            return disease_translation_df['itemLabel_EN'].loc[disease_translation_df['itemLabel_DE']
-                                                              == did_u_mean].tolist()[0]
-        else:
-            complete_disease_name = _complete_partial_words(disease, disease_db_de, disease_db_en)
-            if disease != complete_disease_name:
-                return translate_disease_name(complete_disease_name)
-            else:
-                return disease
+def _add_zero(x):
+    # Adds a zero if day or month only have a single value
+    if len(x) == 1:
+        x = "0" + x
+    return x
 
 
 def clean_country_name(country):
-    """Takes a string of a country/ies (from edb) and returns cleaned country names
-
-    Args:
-        country:
-
-    Returns:
-
-    """
+    # Takes a string of a country/ies (from edb) and returns cleaned country names
 
     if isinstance(country, str):
         card_dir = re.compile(r"(Süd|Nord|West|Ost)\s(\S*)")  # Matches cardinal directions and the string after it
@@ -120,6 +91,39 @@ def clean_country_name(country):
             return country
     elif isinstance(country, list):
         return [clean_country_name(entry) for entry in country]
+
+
+def translate_geonames(to_translate, look_up=get_wiki_countries_df()):
+    if isinstance(look_up, pd.DataFrame):
+        wiki_countries_df = look_up
+    else:
+        wiki_countries_df = get_wiki_countries_df()
+    to_translate = _translate_abbreviation(clean_country_name(to_translate))
+    continents = ["europa", "afrika", "amerika", "australien", "asien"]
+    state_name_de = wiki_countries_df["state_name_de"].tolist()
+    full_state_name_de = wiki_countries_df["full_state_name_de"].tolist()
+    translation = wiki_countries_df["translation_state_name"].tolist()
+    match = None
+
+    if isinstance(to_translate, str):
+        match = _match_country(to_translate, state_name_de, translation)
+        if not match:
+            match = _match_country(to_translate, full_state_name_de, translation)
+        elif not match:
+            match = _match_country(to_translate, translation, translation)
+        # If still no match
+        if not match:
+            did_u_mean = didyoumean.didYouMean(to_translate, state_name_de)
+            if did_u_mean and (did_u_mean.lower() not in continents):
+                try:
+                    match = translate_geonames(did_u_mean)
+                except RecursionError:
+                    print(did_u_mean, "caused recursion error")
+            else:
+                match = to_translate
+    elif isinstance(to_translate, list):
+        match = [translate_geonames(country) for country in to_translate]
+    return match
 
 
 def _translate_abbreviation(to_translate, to_clean=True, look_up=get_wiki_countries_df()):
@@ -156,65 +160,3 @@ def _match_country(country, look_up, translation):
             return translation[look_up.index(identical[0])]
         else:
             return [translation[look_up.index(similar)] for similar in found]
-
-
-def translate_geonames(to_translate, look_up=get_wiki_countries_df()):
-    if isinstance(look_up, pd.DataFrame):
-        wiki_countries_df = look_up
-    else:
-        wiki_countries_df = get_wiki_countries_df()
-    to_translate = _translate_abbreviation(clean_country_name(to_translate))
-    continents = ["europa", "afrika", "amerika", "australien", "asien"]
-    state_name_de = wiki_countries_df["state_name_de"].tolist()
-    full_state_name_de = wiki_countries_df["full_state_name_de"].tolist()
-    translation = wiki_countries_df["translation_state_name"].tolist()
-    match = None
-
-    if isinstance(to_translate, str):
-        match = _match_country(to_translate, state_name_de, translation)
-        if not match:
-            match = _match_country(to_translate, full_state_name_de, translation)
-        elif not match:
-            match = _match_country(to_translate, translation, translation)
-        # If still no match
-        if not match:
-            did_u_mean = didyoumean.didYouMean(to_translate, state_name_de)
-            if did_u_mean and (did_u_mean.lower() not in continents):
-                try:
-                    match = translate_geonames(did_u_mean)
-                except RecursionError:
-                    print(did_u_mean, "caused recursion error")
-            else:
-                match = to_translate
-    elif isinstance(to_translate, list):
-        match = [translate_geonames(country) for country in to_translate]
-    return match
-
-
-def get_cleaned_edb(clean=None, reduced=True, condensed=False, unprocessed=False):
-    """
-
-    Returns:
-        pd.DataFrame: formatted edb
-        if reduce drop columns unnecessary for analysis
-    """
-    if clean is None:
-        clean = [(edb_to_timestamp, [7, 8, 10, 16, 19, 22, 25, 34]),
-                 (translate_geonames, [3, 4]),
-                 (translate_disease_name, [6])]
-    if not isinstance(clean, list):
-        clean = [clean]
-    dirname = os.path.dirname(__file__)
-    path = os.path.join(dirname, 'data', 'edb.csv')
-    edb = pd.read_csv(path, sep=";")
-    edb = edb.dropna(how="all").reset_index(drop=True)
-    edb.columns = list(map(lambda x: x.strip(), edb.columns))
-    if unprocessed:
-        return edb
-    for funct, columns in clean:
-        edb.iloc[:, columns] = edb.iloc[:, columns].applymap(funct)
-    if reduced or condensed:
-        to_drop = edb.iloc[:, [0, 1, 2, 5, 7, 8, 27, 28, 29, 30, 31, 32, 33, 34, 35]].columns.tolist()
-        return edb.drop(to_drop, axis=1)
-    else:
-        return edb
