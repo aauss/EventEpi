8334ad3e79d6e876e7d8efb0eb6fe08bcea99cb4
aauss <a.abbood@live.de>
2019-01-15 16:48:58 +0100

    Working preprocessing of count

--- event_db.py
+++ event_db.py
@@ -10,8 +10,9 @@ def read_cleaned(path=None):
     event_db = _read_unprocessed(path=path)
     event_db_dropped = _rename_and_drop_unused_columns(event_db)
     event_db_nans = _format_missing_data(event_db_dropped)
-    event_db_nans['date_of_data'] = event_db_nans['date_of_data'].apply(_to_timestamp)
-    return event_db_nans
+    event_db_time_format = _to_datetime(event_db_nans)
+    event_db_count_format = _clean_count(event_db_time_format)
+    return event_db_count_format
 
 
 def _read_unprocessed(path=None):
@@ -31,6 +32,7 @@ def _rename_and_drop_unused_columns(event_db):
                        'Fälle gesamt*']
     columns_to_keep.extend(URLs)
     event_db = event_db[columns_to_keep]
+    # Rename columns
     event_db.columns = ['country_edb', 'disease_edb', 'date_of_data', 'count_edb', 'URL_1', 'URL_2', 'URL_3', 'URL_4']
     return event_db
 
@@ -46,22 +48,26 @@ def _format_missing_data(event_db):
     return event_db
 
 
-def _to_timestamp(date):
-    date_matched = re.match(r"(\d{1,2})\D(\d{1,2})\D(\d{4})", str(date))
-    if date_matched:
-        day = int(date_matched[1])
-        month = int(date_matched[2])
-        year = int(date_matched[3])
+def _to_datetime(event_db):
+    event_db.date_of_data = event_db.date_of_data.apply(lambda x:
+                                                        pd.to_datetime(x,
+                                                                       dayfirst=True,
+                                                                       errors='coerce'))
+    return event_db
 
-        try:
-            time_stamp = pd.Timestamp(year=int(year), month=int(month), day=int(day))
-        except (TypeError, UnknownTimeZoneError):
-            time_stamp = pd.NaT
-            warnings.warn(f'{date} is not an existing date. Please change it')
+
+def _clean_count(event_db):
+    event_db.count_edb = event_db.count_edb.replace(['.', '.'], ['', ''])
+    event_db.count_edb = event_db.count_edb.apply(_keep_only_integers)
+    return event_db
+
+
+def _keep_only_integers(string_with_int):
+    if isinstance(string_with_int, str):
+        int_as_string = re.search(r'(\d)+', string_with_int)[0]
+        return int(int_as_string)
     else:
-        time_stamp = pd.NaT
-    return time_stamp
+        return string_with_int
 
 
-def _to_count(count):
-    pass
+
