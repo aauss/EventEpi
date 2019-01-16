7afef9cc309f4a0a2350a17743c9a35c60b6278e
aauss <a.abbood@live.de>
2019-01-16 18:28:54 +0100

    Add working sparql query for disease lookup

---
+++ nlp_surveillance/wikidata_disease_names/query.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+def get_wikidata_disease_df():
+    endpoint_url = "https://query.wikidata.org/sparql"
+    query = """SELECT Distinct ?item ?itemLabel_DE ?itemLabel_EN WHERE {
+                    ?item wdt:P31 wd:Q12136.
+                    OPTIONAL{
+                    ?item rdfs:label ?itemLabel_DE.
+                    FILTER (lang(?itemLabel_DE) = "de"). }
+                    ?item rdfs:label ?itemLabel_EN.
+                    FILTER (lang(?itemLabel_EN) = "en").
+                    } order by ?item"""
+    disease_translation_df = _get_results_sparql(endpoint_url, query)
+    return disease_translation_df
+
+
+def _get_results_sparql(endpoint_url, query):
+    sparql = SPARQLWrapper(endpoint_url)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    df = pd.DataFrame(sparql.query().convert()["results"]["bindings"])
+    return df.applymap(lambda x: x['value'] if isinstance(x, dict) else x)
