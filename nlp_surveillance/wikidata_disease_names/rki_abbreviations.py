d29a3764d455fcc812728dd4341bf7d91b373d1f
aauss <a.abbood@live.de>
2019-01-17 15:44:42 +0100

    Change path, add todo, and rename columns for merge in event db pipeline

--- nlp_surveillance/wikidata_disease_names/rki_abbreviations.py
+++ nlp_surveillance/wikidata_disease_names/rki_abbreviations.py
@@ -2,9 +2,11 @@ import os
 import pandas as pd
 
 
-def _get_codes():
+def get_RKI_abbreviations():
+    # TODO: Maybe think about a cleaner function for the German disease names (Röteln, konnatal -> Röteln)
     dirname = os.path.dirname(__file__)
-    path = os.path.join(dirname, '..', '..', 'rki', 'disease_code.csv')
+    path = os.path.join(dirname, '..', '..', 'data', 'rki', 'disease_codes.csv')
     disease_code_df = pd.read_csv(path, ';')
     disease_code_df = disease_code_df[['Code', 'TypeName']]
+    disease_code_df = disease_code_df.rename(columns={'TypeName': 'itemLabel_DE'})  # Rename for later merging
     return disease_code_df
