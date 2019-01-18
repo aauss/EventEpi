3ab6aebb3dac7b3bfd41e0553d754e934e73da4a
aauss <a.abbood@live.de>
2019-01-18 13:49:23 +0100

    Rename function and add translation tuple

--- nlp_surveillance/wikidata_disease_names/lookup.py
+++ nlp_surveillance/wikidata_disease_names/lookup.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def merge_disease_lookup(disease_lookup, rki_abbreviations):
+def merge_disease_lookup_as_dict(disease_lookup, rki_abbreviations):
     lookup_with_abbreviations = _merge(disease_lookup, rki_abbreviations)
     as_dict = _to_translation_dict(lookup_with_abbreviations)
     return as_dict
@@ -10,10 +10,13 @@ def merge_disease_lookup(disease_lookup, rki_abbreviations):
 def _to_translation_dict(disease_lookup):
     de_to_en = list(zip(disease_lookup.itemLabel_DE,
                         disease_lookup.itemLabel_EN))
+    en_to_en = list(zip(disease_lookup.itemLabel_DE,
+                        disease_lookup.itemLabel_DE))
     abbreviation_to_en = list(zip(disease_lookup.abbreviation,
                                   disease_lookup.itemLabel_EN))
-    de_to_en.extend(abbreviation_to_en)
-    translation_dict = dict(de_to_en)
+    de_to_en.extend(en_to_en)
+    abbreviation_to_en.extend(de_to_en)
+    translation_dict = dict(abbreviation_to_en)
     translation_dict = {k: v for k, v in translation_dict.items() if None not in [k, v]}
     return translation_dict
 
