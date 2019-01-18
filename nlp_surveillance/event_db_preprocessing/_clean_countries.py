88e2517278a8b88ed971a4eeb815eee6b4f667c1
aauss <a.abbood@live.de>
2019-01-18 14:00:16 +0100

    Migrate function into utils and call if form there

--- nlp_surveillance/event_db_preprocessing/_clean_countries.py
+++ nlp_surveillance/event_db_preprocessing/_clean_countries.py
@@ -1,12 +1,12 @@
 import re
 import warnings
-import pandas as pd
-import numpy as np
+
+import utils
 
 
 def clean_countries(event_db):
     event_db.country_edb = event_db.country_edb.apply(_clean_country_str)
-    event_db = _split_country_by_comma(event_db)
+    event_db = utils.split_strings_at_comma_and_distribute_to_new_rows(event_db, 'country_edb')
     return event_db
 
 
@@ -22,15 +22,6 @@ def _clean_country_str(country):
     return country
 
 
-def _split_country_by_comma(event_db):
-    split, length = _split_by_comma_and_then_calc_len_of_split(event_db)
-    repeated_entries_dict = _repeat_entries_as_dict(event_db, length)
-    repeated_entries_dict['country_edb'] = np.concatenate(split.values)
-    split_event_db = pd.DataFrame(repeated_entries_dict, columns=repeated_entries_dict.keys())
-    split_event_db.country_edb = split_event_db.country_edb.str.strip()
-    return split_event_db
-
-
 def _correct_wrong_use_of_cardinal_directions(country):
     cardinal_dir = re.compile(r"(Süd|Nord|West|Ost)\s(\S*)")
     if cardinal_dir.match(country) and country.lower() != 'korea':
@@ -40,22 +31,5 @@ def _correct_wrong_use_of_cardinal_directions(country):
         except IndexError:
             warnings.warn('Problems with processing country string with cardinal direction in name')
     return country
-
-
-def _split_by_comma_and_then_calc_len_of_split(event_db):
-    split = event_db.country_edb.str.split(',')
-    # Nones in country column cause non-list entries that need to be transformed to list
-    split = pd.Series([country if isinstance(country, list) else [country]
-                       for country in split])
-
-    length = split.str.len()
-    length = [1 if np.isnan(i) else int(i) for i in length]  # Also caused by Nones in country column
-    return split, length
-
-
-def _repeat_entries_as_dict(event_db, length):
-    columns = event_db.columns
-    return {column: np.repeat(event_db[column].values, length) for column in columns
-            if column != 'country_edb'}
 
 
