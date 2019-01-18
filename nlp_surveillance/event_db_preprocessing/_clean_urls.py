57447728c412a87afe1549510f784ec4d23c8855
aauss <a.abbood@live.de>
2019-01-18 16:43:33 +0100

    Change order of cleaning steps and add function to verify URL's adress validity

--- nlp_surveillance/event_db_preprocessing/_clean_urls.py
+++ nlp_surveillance/event_db_preprocessing/_clean_urls.py
@@ -5,7 +5,7 @@ import utils
 
 def clean_urls(event_db):
     event_db = utils.split_strings_at_comma_and_distribute_to_new_rows(event_db, 'URL_1')
-    event_db.URL_1 = event_db.URL_1.apply(_remove_guillemets())
+    event_db.URL_1 = event_db.URL_1.apply(_remove_guillemets).apply(_check_url_validity)
     return event_db
 
 
@@ -18,3 +18,19 @@ def _remove_guillemets(url):
     except TypeError:
         url = None
     return url
+
+
+def _check_url_validity(url):
+    # Inspired from django
+    regex = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    try:
+        is_valid_url = re.match(regex, url) is not None
+        return is_valid_url
+    except TypeError:
+        return None
