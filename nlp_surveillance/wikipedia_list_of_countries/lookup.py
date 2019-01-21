e763ddaa504056d59716d55cab5971e369034221
aauss <a.abbood@live.de>
2019-01-21 18:34:25 +0100

    Sort columns before creating dict to prioritize certain columns over other as keys. Rename variables in abbreviation function

--- nlp_surveillance/wikipedia_list_of_countries/lookup.py
+++ nlp_surveillance/wikipedia_list_of_countries/lookup.py
@@ -1,44 +1,31 @@
 import re
 import pandas as pd
 
+from utils import my_utils
+
 
 def to_translation_dict(country_lookup_df):
-    columns = country_lookup_df.columns
-    directions_to_translate_to = [(other_column, 'translation_state_name')
-                                  for other_column in columns]
-    lists_of_translation_tuples = [list(zip(country_lookup_df[from_lang_x], country_lookup_df[to_lang_y]))
-                                   for from_lang_x, to_lang_y in directions_to_translate_to]
-    flattened = [item for sublist in lists_of_translation_tuples for item in sublist]
+    # Important to use the sorted columns, so that the custom abbreviations are put into the dict first and are
+    # overwritten by other input that has more priority
+    columns = sorted(country_lookup_df.columns)
+    lists_of_translation_tuples = [list(zip(country_lookup_df[other_column],
+                                            country_lookup_df['translation_state_name']))
+                                   for other_column in columns]
+    flattened = my_utils.flatten_list(lists_of_translation_tuples)
     country_lookup_dict = dict(flattened)
-    country_lookup_withou_nones = {k: v for k, v in country_lookup_dict.items() if None not in [k, v]}
-    return country_lookup_withou_nones
-
-
-def clean_wikipedia_countries(wikipedia_country_df):
-    wiki_df_without_brackets = wikipedia_country_df.applymap(lambda x: re.sub(r"\(.*\)", " ", str(x)))
-    wiki_df_without_footnotes = wiki_df_without_brackets.applymap(lambda x: re.sub(r'\[\d*\]', "", x))
-    wiki_df_without_dash = wiki_df_without_footnotes.applymap(_replace_wiki_dash_with_none)
-    return wiki_df_without_dash
-
-
-def _replace_wiki_dash_with_none(string):
-    dash = u"\u2014"  # Used dash for missing entry in Wikipedia table
-    if dash == string:
-        return None
-    else:
-        return string
+    country_lookup_without_nones = {k: v for k, v in country_lookup_dict.items() if None not in [k, v]}
+    return country_lookup_without_nones
 
 
 def abbreviate_wikipedia_country_df(wikipedia_country_df):
     """Search for names that might have abbreviations. If they consist of two or more words that start with a capital
     letter, it makes an abbreviation out of it
     """
-
     columns_to_abbreviate = ["state_name_de", "full_state_name_de", "translation_state_name"]
-    abbreviated_columns = [list(map(_abbreviate_country, wikipedia_country_df[column].tolist()))
-                           for column in columns_to_abbreviate]
-    merged_columns = [list(a) for a in zip(*abbreviated_columns)]
-    custom_abbreviations = pd.DataFrame(merged_columns,
+    list_of_abbreviated_columns_as_lists = [list(map(_abbreviate_country, wikipedia_country_df[column].tolist()))
+                                            for column in columns_to_abbreviate]
+    merged_columns_as_list_of_rows = [list(row) for row in zip(*list_of_abbreviated_columns_as_lists)]
+    custom_abbreviations = pd.DataFrame(merged_columns_as_list_of_rows,
                                         columns=['custom_abbreviation_state_name_de',
                                                  'custom_abbreviation_full_state_name_de',
                                                  'custom_abbreviation_translation_state_name'])
