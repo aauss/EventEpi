c5dc30776d6d92b72396350b48b637d22fa97d0a
aauss <a.abbood@live.de>
2019-01-22 14:24:31 +0100

    Add working who and promed scraper

---
+++ scraper/promed_scraper.py
@@ -0,0 +1,30 @@
+import requests
+import re
+
+from utils import my_utils
+
+
+def scrape(list_of_years, proxy=None):
+    # If from_year and to_year are the same, all URLs of of all articles of this year are scraped
+    ids = _get_article_ids_per_year(from_year=min(list_of_years),
+                                    to_year=max(list_of_years),
+                                    proxy=proxy)
+    parsed = [f'https://www.promedmail.org/post/{str(id_)}' for id_ in ids]
+    return parsed
+
+
+def _get_article_ids_per_year(from_year='2018', to_year='2018', proxy=None):
+    _get_content_of_search_page = lambda x: (requests.get(f'https://www.promedmail.org/ajax/runSearch.php?'
+                                                          f'pagenum={x}&kwby1=summary&'
+                                                          f'search=&date1=01/01/{from_year}&'
+                                                          f'date2=01/01/2019&feed_id=1&submit=next',
+                                                          proxies=proxy)
+                                             .content
+                                             .decode('utf-8'))
+
+    content = _get_content_of_search_page(0)
+    max_page_num = re.search(r'Page \d+ of (\d+)', content)[1]
+    ids_of_pages = [re.findall(r'id\d+', _get_content_of_search_page(i))
+                    for i in range(int(max_page_num))]
+    ids = my_utils.flatten_list(ids_of_pages)
+    return ids
---
+++ scraper/who_scraper.py
@@ -0,0 +1,58 @@
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+
+from utils import my_utils
+
+
+def scrape(list_of_years=None, months=None, headers=None, proxy=None):
+    # Scrapes the WHO DONs using the WHO DON scraping functions and returns the links to these DONs
+
+    if not _connection_is_possible() and proxy is not None:
+        headers = my_utils.load_rki_header_and_proxy_dict()['headers']
+        proxy = my_utils.load_rki_header_and_proxy_dict()['proxy']
+
+    list_of_years = _get_links_by_year(list_of_years=list_of_years, proxy=proxy, headers=headers)
+    all_links = _get_links_per_year(list_of_years, list_of_months=months, proxy=proxy, headers=headers)
+    return all_links
+
+
+def _get_links_by_year(list_of_years=None, proxy=None, headers=None):
+    # Returns (all) the anual links of the WHO DONs
+
+    page = requests.get('http://www.who.int/csr/don/archive/year/en/', proxies=proxy, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    archive_years = soup.find('ul', attrs={'class': 'list'})
+    years_links_html = archive_years.find_all('a')
+    if list_of_years:
+        return ['http://www.who.int' + link.get('href') for link in years_links_html if
+                any(year in link for year in list_of_years)]
+    else:
+        return ['http://www.who.int' + link.get('href') for link in years_links_html]
+
+
+def _get_links_per_year(years_links, list_of_months=None, proxy=None, headers=None):
+    # Take a list of links to the annual archive and return a list of DON links of these years
+
+    all_links = []
+
+    for year_link in years_links:
+        page_year = requests.get(year_link, proxies=proxy, headers=headers)
+        soup_year = BeautifulSoup(page_year.content, 'html.parser')
+        archive_year = soup_year.find('ul', attrs={'class': 'auto_archive'})
+        daily_links = ['http://www.who.int' + link.get('href') for link in archive_year.find_all('a')]
+        all_links.extend(daily_links)
+
+    if list_of_months:
+        all_links = [link for link in all_links if
+                     any(month in link for month in map(lambda s: s.lower(), list_of_months))]
+    return all_links
+
+
+def _connection_is_possible():
+    try:
+        urllib.request.urlopen('https://www.rki.de/DE/Home/homepage_node.html', timeout=4)
+        return True
+    except urllib.request.URLError:
+        return False
+
