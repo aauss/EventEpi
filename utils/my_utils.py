3082f1bf54ccee22282d481dff8789c6bd0f33f1
aauss <a.abbood@live.de>
2019-01-23 15:20:41 +0100

    Allow split to also use other parameter than comma to split at. Add function to load proxy settings

--- utils/my_utils.py
+++ utils/my_utils.py
@@ -1,3 +1,4 @@
+import pickle
 import pandas as pd
 import numpy as np
 
@@ -6,8 +7,13 @@ def flatten_list(to_flatten):
     return [item for sublist in to_flatten for item in sublist]
 
 
-def split_strings_at_comma_and_distribute_to_new_rows(df, column):
-    split, length = _split_by_comma_and_then_calc_len_of_split(df, column)
+def load_rki_header_and_proxy_dict():
+    # TODO: when proxy is needed again, store this pickle in data/rki
+    return pickle.load(open('scraping_params.p', 'rb'))
+
+
+def split_strings_at_comma_and_distribute_to_new_rows(df, column, split_by=','):
+    split, length = _split_by_comma_and_then_calc_len_of_split(df, column, split_by)
     repeated_entries_dict = _repeat_entries_as_dict(df, length, column)
     repeated_entries_dict[column] = np.concatenate(split.values)
     split_df = pd.DataFrame(repeated_entries_dict, columns=repeated_entries_dict.keys())
@@ -15,8 +21,8 @@ def split_strings_at_comma_and_distribute_to_new_rows(df, column):
     return split_df
 
 
-def _split_by_comma_and_then_calc_len_of_split(df, column):
-    split = df[column].str.split(',')
+def _split_by_comma_and_then_calc_len_of_split(df, column, split_by):
+    split = df[column].str.split(split_by)
     # Nones in column cause non-list entries that need to be transformed to list
     split = pd.Series([entry if isinstance(entry, list) else [entry]
                        for entry in split])
@@ -27,6 +33,6 @@ def _split_by_comma_and_then_calc_len_of_split(df, column):
 
 
 def _repeat_entries_as_dict(df, length, split_column):
-    columns = df.columns
-    return {column: np.repeat(df[column].values, length) for column in columns
-            if column != split_column}
+    repeated_entries_as_dict = {column: np.repeat(df[column].values, length) for column in df.columns
+                                if column != split_column}
+    return repeated_entries_as_dict
