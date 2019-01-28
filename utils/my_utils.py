a30f3ffc06b6d9ff139bf3a1750bb6b22f1b3359
aauss <a.abbood@live.de>
2019-01-28 18:14:57 +0100

    Add functino to distribute to new rows when there is already a list

--- utils/my_utils.py
+++ utils/my_utils.py
@@ -26,24 +26,38 @@ def load_rki_header_and_proxy_dict():
     return pickle.load(open('scraping_params.p', 'rb'))
 
 
-def split_strings_at_comma_and_distribute_to_new_rows(df, column, split_by=','):
-    split, length = _split_by_comma_and_then_calc_len_of_split(df, column, split_by)
-    repeated_entries_dict = _repeat_entries_as_dict(df, length, column)
-    repeated_entries_dict[column] = np.concatenate(split.values)
+def split_strings_at_comma_and_distribute_to_new_rows(df, split_column, split_by=','):
+    list_of_splits_as_series = _split_by_comma(df, split_column, split_by)
+    len_of_splits = _get_length_of_split(list_of_splits_as_series)
+    repeated_entries_dict = _repeat_entries_as_dict(df, len_of_splits, split_column)
+    repeated_entries_dict[split_column] = np.concatenate(list_of_splits_as_series.values)
     split_df = pd.DataFrame(repeated_entries_dict, columns=repeated_entries_dict.keys())
-    split_df.loc[:, column] = split_df.loc[:, column].str.strip()
+    split_df.loc[:, split_column] = split_df.loc[:, split_column].str.strip()
     return split_df
 
 
-def _split_by_comma_and_then_calc_len_of_split(df, column, split_by):
+def split_list_and_distribute_to_new_rows(df, split_column):
+    repeated_entries_dict = _repeat_entries_as_dict(df, df[split_column].apply(len), split_column)
+
+    # np.concatenate does not work with tuples
+    repeated_entries_dict[split_column] = flatten_list(df[split_column].values)
+    split_df = pd.DataFrame(repeated_entries_dict, columns=repeated_entries_dict.keys())
+    return split_df
+
+
+def _split_by_comma(df, column, split_by):
     split = df[column].str.split(split_by)
+
     # Nones in column cause non-list entries that need to be transformed to list
     split = pd.Series([entry if isinstance(entry, list) else [entry]
                        for entry in split])
+    return split
 
+
+def _get_length_of_split(split):
     length = split.str.len()
-    length = [1 if np.isnan(i) else int(i) for i in length]  # Also caused by Nones in column
-    return split, length
+    length = [1 if np.isnan(i) else int(i) for i in length]  # Necessary because there are Nones in column
+    return length
 
 
 def _repeat_entries_as_dict(df, length, split_column):
