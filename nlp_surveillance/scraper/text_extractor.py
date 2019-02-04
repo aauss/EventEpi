fc9cfabf71f26bf67f908e4874e4d540544f585c
aauss <a.abbood@live.de>
2019-02-04 17:42:32 +0100

    Add function to retrieve the original html content of post

--- nlp_surveillance/scraper/text_extractor.py
+++ nlp_surveillance/scraper/text_extractor.py
@@ -2,6 +2,7 @@ import tika
 import logging
 import unicodedata
 import re
+import requests
 from tika import parser
 from boilerpipe.extract import Extractor
 from urllib.error import URLError
@@ -9,6 +10,7 @@ from socket import timeout
 
 
 def extract_cleaned_text_from_url(url):
+    kwargs = {'url': url}
     if url is None:
         return url
     else:
@@ -16,7 +18,10 @@ def extract_cleaned_text_from_url(url):
             tika.TikaClientOnly = True
             extracted = _extract_cleaned_text_from_pdf(url)
         else:
-            extracted = _extract_cleaned_text_from_html_webpage(url)
+            if 'promed' in url:
+                html = _get_html_from_promed_url(url)
+                kwargs = {'html': html}
+            extracted = _extract_cleaned_text_from_html_webpage(**kwargs)
         return _remove_control_characters(extracted)
 
 
@@ -33,19 +38,26 @@ def _extract_cleaned_text_from_pdf(url):
     return text
 
 
-def _extract_cleaned_text_from_html_webpage(url):
+def _extract_cleaned_text_from_html_webpage(**kwargs):
     try:
-        text = Extractor(extractor='ArticleExtractor', url=str(url)).getText()
+        text = Extractor(extractor='ArticleExtractor', **kwargs).getText()
     except (UnicodeDecodeError, URLError, ValueError, timeout) as e:
-        print(f'{url} caused {e}')
+        print(e)
         text = None
     return text
+
+
+def _get_html_from_promed_url(url):
+    id_ = re.search(r'(\d+)', url)[0]
+    url = f'http://www.promedmail.org/ajax/getPost.php?alert_id={id_}'
+    ajax_request_for_post_as_html = requests.get(url, headers={"Referer": "http://www.promedmail.org/"}).json().get('post')
+    return ajax_request_for_post_as_html
 
 
 def _remove_control_characters(string):
     if string is None:
         return string
     else:
-        string = "".join(char for char in string if unicodedata.category(char)[0] != "C")
+        string = "".join(char if unicodedata.category(char)[0] != "C" else ' ' for char in string)
         string_without_double_spaces = re.sub(r'(\s){2,}', ' ', string)  # Spaces added after removal of characters
         return string_without_double_spaces
