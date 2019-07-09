5a7092aea00d63f1ed80761fb790868d0e77de79
Auss Abbood <AbboodA@rki.de>
2019-07-09 14:07:38 +0200

    Add doc strings and make cleam_promed_urls easier to udnerstand

--- nlp_surveillance/event_db_preprocessing/clean_urls.py
+++ nlp_surveillance/event_db_preprocessing/clean_urls.py
@@ -4,6 +4,17 @@ from nlp_surveillance import my_utils
 
 
 def clean_urls(event_db):
+    """Cleans formatting errors and changes design of URL columns in incident database
+
+    Merges all URL columns to one single URL column. If one row has many entries in different
+    URL columns, the row is repeated with the same values except for the URL. Also, the function
+    removes formatting errors that were found during exploration of incident database
+    Args:
+        event_db (pd.DataFrame): Incident database with formatting errors and seperate URL columns
+
+    Returns (pd.DataFrame): Incident database without formatting errors and merged URL columns
+
+    """
     event_db = _combine_url_columns_row_wise_with_comma(event_db)
     event_db = my_utils.split_strings_at_comma_and_distribute_to_new_rows(event_db, 'URL')
     event_db['URL'] = event_db['URL'].str.strip()
@@ -35,7 +46,6 @@ def _remove_guillemets(url):
 
 
 def _only_keep_valid_urls(url):
-
     necessary = ['http', 'www.', '.org', '.com', '.int']
     if (url is not None) and any(nec in url for nec in necessary):
         return url
@@ -47,10 +57,12 @@ def _clean_promed_urls(url):
     # Necessary since there are many URL writings for the same article
     if (url is not None) and 'promedmail' in url:
         url = url.replace('http:', 'https:')
-        no_www_match = re.match(r'(https://)(promedmail.org/post/.*)', url)
-        if no_www_match:
-            url = no_www_match[1] + 'www.' + no_www_match[2]
-        url = re.sub(r'(https://www.promedmail.org/post/)\d+\.(\d+)', r'\1\2', url)
+        no_www_in_url = re.match(r'(https://)(promedmail.org/post/.*)', url)
+        if no_www_in_url:
+            url = no_www_in_url[1] + 'www.' + no_www_in_url[2]
+        url_without_non_id_number = re.sub(r'(https://www.promedmail.org/post/)\d+\.(\d+)', r'\1\2', url)
         if 'direct.php?' in url:
-            url = re.sub(r'(https://www.promedmail.org/)direct\.php\?id=\d+\.(\d+)', r'\1\2', url)
-    return url
+            url_without_non_id_number = re.sub(r'(https://www.promedmail.org/)direct\.php\?id=\d+\.(\d+)', r'\1\2', url)
+        return url_without_non_id_number
+    else:
+        return url
