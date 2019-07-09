7f04ee90161b78ba81b09db82bca402472cdfaa8
Auss Abbood <AbboodA@rki.de>
2019-07-09 10:53:30 +0200

    Add type hints and fix indentation of function

--- nlp_surveillance/scraper/promed_scraper.py
+++ nlp_surveillance/scraper/promed_scraper.py
@@ -8,9 +8,8 @@ from tqdm import tqdm_notebook as tqdm
 from nlp_surveillance.scraper.text_extractor import get_html_from_promed_url
 
 
-def scrape(from_date, to_date, proxy=None):
+def scrape(from_date: str, to_date: str, proxy=None) -> pd.DataFrame:
     # Date in the form mm/dd/YYYY
-    # If from_year and to_year are the same, all URLs of of all articles of this year are scraped
     ids = _get_article_ids_per_year(from_date=from_date,
                                     to_date=to_date,
                                     proxy=proxy)
@@ -19,7 +18,7 @@ def scrape(from_date, to_date, proxy=None):
     return urls
 
 
-def _get_article_ids_per_year(from_date='01/01/2018', to_date='12/31/2018', proxy=None):
+def _get_article_ids_per_year(from_date='01/01/2018', to_date='12/31/2018', proxy=None) -> list:
     # date in the format mm/dd/YYYY
     # adjust from_date to valid smallest date
     from_date = _correct_to_earliest_allowed_date(from_date)
@@ -44,13 +43,13 @@ def _get_article_ids_per_year(from_date='01/01/2018', to_date='12/31/2018', prox
     return ids
 
 
-def _correct_to_earliest_allowed_date(from_date):
+def _correct_to_earliest_allowed_date(from_date: str) -> str:
     corrected_date = max(pd.Timestamp(day=int(from_date[3:5]), month=int(from_date[0:2]), year=int(from_date[6:11])),
                          pd.Timestamp(day=20, month=8, year=1994))
     return corrected_date.strftime('%m/%d/%Y')
 
 
-def _get_content_of_search_page(from_date, to_date, page_num, proxy):
+def _get_content_of_search_page(from_date: str, to_date: str, page_num: int, proxy: dict) -> str:
     return (requests
             .get(f'https://www.promedmail.org/ajax/runSearch.php?'
                  f'pagenum={page_num}&'
@@ -62,9 +61,9 @@ def _get_content_of_search_page(from_date, to_date, page_num, proxy):
             )
 
 
-def _recursively_call_with_last_scraped_date_as_new_to_date(last_id_before_error, from_date):
-        html = get_html_from_promed_url(last_id_before_error)
-        published_date = re.search(r'Published Date:.* (\d\d\d\d-\d\d-\d\d)', html)[1]
-        last_date_as_mm_dd_yyyy = f'{published_date[5:7]}/{published_date[8:10]}/{published_date[0:4]}'
-        return _get_article_ids_per_year(from_date=from_date, to_date=last_date_as_mm_dd_yyyy)
+def _recursively_call_with_last_scraped_date_as_new_to_date(last_id_before_error, from_date: str):
+    html = get_html_from_promed_url(last_id_before_error)
+    published_date = re.search(r'Published Date:.* (\d\d\d\d-\d\d-\d\d)', html)[1]
+    last_date_as_mm_dd_yyyy = f'{published_date[5:7]}/{published_date[8:10]}/{published_date[0:4]}'
+    return _get_article_ids_per_year(from_date=from_date, to_date=last_date_as_mm_dd_yyyy)
 
