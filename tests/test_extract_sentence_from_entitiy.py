1820d9d126531be65aa977756b79aa90fb3cbb2a
Auss Abbood <AbboodA@rki.de>
2019-07-12 15:13:57 +0200

    Transform test to test counts (which is used) instead of geonames (not used for pipeline)

--- tests/test_extract_sentence_from_entitiy.py
+++ tests/test_extract_sentence_from_entitiy.py
@@ -3,8 +3,8 @@ import os
 import pickle
 import sys
 
-from nlp_surveillance.classifier.extract_sentence import extract_sentence_from_found_entities
-from utils.my_utils import delete_non_epitator_name_entity_tiers
+from nlp_surveillance.classifier.extract_sentence import extract_entities_with_sentence
+from nlp_surveillance.my_utils import delete_non_epitator_name_entity_tiers
 
 sys.setrecursionlimit(5000)  # Otherwise serialization does not work
 
@@ -12,16 +12,14 @@ sys.setrecursionlimit(5000)  # Otherwise serialization does not work
 @pytest.fixture
 def annotated_example():
     dirname = os.path.dirname(__file__)
-    print(dirname)
     path = os.path.join(dirname, '..', 'data', 'fixtures', 'annotated_example.pkl')
     if not os.path.isfile(path):
         from epitator.annotator import AnnoDoc
-        from epitator.geoname_annotator import GeonameAnnotator
+        from epitator.count_annotator import CountAnnotator
 
-        annotated = AnnoDoc('I am in Berlin. Something is odd in Berlin.'
-                            ' Still, I seem to like Berlin more than Frankfurt now.'
-                            ' I am sad that I am not in Switzerland.')
-        annotated.add_tiers(GeonameAnnotator())
+        annotated = AnnoDoc('I am in Berlin. Here are 5 confirmed cases of influenza. '
+                            'Still, less worse than those 100 confirmed and 200 suspected cases last year.')
+        annotated.add_tiers(CountAnnotator())
         annotated = delete_non_epitator_name_entity_tiers(annotated)
         os.makedirs(os.path.dirname(path), exist_ok=True)
         with open(path, 'wb') as handel:
@@ -34,10 +32,8 @@ def annotated_example():
 
 def test_sentence_extraction(annotated_example):
     # For each entity found, I expect the sentence it occurs to be returned
-    sentences_expected = ['I am in Berlin.',
-                          'Something is odd in Berlin.',
-                          'Still, I seem to like Berlin more than Frankfurt now.',
-                          'Still, I seem to like Berlin more than Frankfurt now.',
-                          'I am sad that I am not in Switzerland.']
-    sentences_example = extract_sentence_from_found_entities(annotated_example, 'geonames')
+    sentences_expected = ['Here are 5 confirmed cases of influenza.',
+                          'Still, less worse than those 100 confirmed and 200 suspected cases last year.',
+                          'Still, less worse than those 100 confirmed and 200 suspected cases last year.']
+    _, sentences_example = extract_entities_with_sentence(annotated_example, 'counts')
     assert sentences_example == sentences_expected
